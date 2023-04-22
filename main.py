2bbc04a56b596587f3d33829f6a39dc9b7074d79
steven <stevenvuong96@gmail.com>
2023-04-22 15:20:47 +0800

    make asynchronous querying

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import os
 from pathlib import Path
+from langchain.embeddings.openai import OpenAIEmbeddings
+from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
 
 import pandas as pd
 import pinecone
 from dotenv import load_dotenv
+import asyncio
+
 
 # load environment variables
 load_dotenv()
@@ -12,6 +17,11 @@ load_dotenv()
 pinecone.init(
     api_key=os.getenv("PINECONE_API_KEY"), environment=os.getenv("PINECONE_ENVIRONMENT")
 )
+embeddings_model = OpenAIEmbeddings(model="text-embedding-ada-002")
+
+
+def embed_text(text):
+    return embeddings_model.embed_query(text)
 
 
 if __name__ == "__main__":
@@ -40,10 +50,30 @@ if __name__ == "__main__":
         # drop unwanted columns
         df.drop(["headline", "brief", "updateTime", "date"], axis=1, inplace=True)
 
-        print(df.head())
+        # just do head for now; delete this line later
+        df = df.head(2)
+        print("-" * 80)
+        print(df.head()))
 
+        # loop through and embed queries; no support for documents at the moment
+        with ThreadPoolExecutor() as executor:
+            embedded_texts = list(executor.map(embed_text, df["text"].values))
+
+        # create pinecone list to insert into index of [("id", [vector], {"ordinal":})]
+        pinecone_upsert_list = list(
+            zip(
+                df["id"].tolist(),
+                embedded_texts,
+                [{"ordinal": x} for x in df["ordinal"].tolist()],
+            )
+        )
+
+        # upsert pinecone index
+        pinecone_index.upsert(pinecone_upsert_list)
+
+        # Get index statistics
+        stats = pinecone_index.describe_index_stats()
+        print("-" * 80)
+        print("Index statistics:")
+        print(stats)
         break
-
-    # convert xlsx to csv file
-
-    # read csv files from csv_dir
